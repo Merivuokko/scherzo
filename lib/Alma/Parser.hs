638b8a88c920e18dec84359182d76404c00739bb
Aura Kelloniemi <kaura.dev@sange.fi>
2021-12-03 15:58:31 +0200

    Export configuration routines from Alma.Parser
    Specifically ParserConfig data type and readConfigFile function.

--- lib/Alma/Parser.hs
+++ lib/Alma/Parser.hs
@@ -2,8 +2,11 @@
 
 module Alma.Parser
     (Parser,
+     ParserConfig,
+     readConfigFile,
      parseMusic)
 where
 
+import Alma.Parser.Config
 import Alma.Parser.Rules
 import Alma.Parser.Type
