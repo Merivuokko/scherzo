3749be3b2afe8fa869d20f564892facc1798476a
Aura Kelloniemi <kaura.dev@sange.fi>
2021-12-08 15:37:29 +0200

    Combine lib/alma/Syntax/{Config,State}.hs
    Move all content from Alma.Syntax.State to Alma.Syntax.Config and improve the configuration module to support state machine parsing.

--- lib/Alma/Syntax/Config.hs
+++ lib/Alma/Syntax/Config.hs
@@ -3,27 +3,58 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE StrictData #-}
 
--- | Syntax command definitions
+-- | Syntax configuration
 
-module Alma.Syntax.State
-    (SStateName,
-    SState,
-    Command)
+module Alma.Syntax.Config
+    (SyntaxConfig (..),
+     TokenMap (map, trie),
+     readConfigFile,
+     SStateName,
+     SState,
+     Command)
 where
 
 import Data.Kind (Type)
+import Data.HashMap.Strict qualified as HM
+import Data.Text qualified as T
 import Dhall
 
+import Alma.Parser.CharTrie as CT
+
+type SyntaxConfig :: Type
+data SyntaxConfig = SyntaxConfig {
+    states :: HM.HashMap SStateName SState,
+    initState :: SStateName
+    }
+    deriving stock (Eq, Generic, Show)
+    deriving anyclass (FromDhall)
+
+type TokenMap :: Type -> Type
+data TokenMap v = TokenMap {
+    map :: HM.HashMap T.Text v,
+    trie :: CT.CharTrie v
+    }
+    deriving stock (Eq, Show)
+
+instance (FromDhall v) => FromDhall (TokenMap v) where
+    autoWith _ = mkTokenMap <$> hashMap strictText auto
+
+mkTokenMap :: forall v. HM.HashMap T.Text v -> TokenMap v
+mkTokenMap m = TokenMap m (CT.fromHashMap m)
+
+readConfigFile :: FilePath -> IO SyntaxConfig
+readConfigFile file = inputFile auto file
+
 type SStateName :: Type
-type SStateName = Text
+type SStateName = T.Text
 
 type SState :: Type
 data SState = SState {
     init :: Command,
     fail :: Command,
-    tokens :: [Command]
+    tokens :: TokenMap Command
     }
-    deriving stock (Eq, Generic, Ord, Read, Show)
+    deriving stock (Eq, Generic, Show)
     deriving anyclass (FromDhall)
                       
 type Command :: Type
@@ -32,6 +63,6 @@ data Command
     | LeaveState
     | Finish
     | Fail
-    | Write Text
-    deriving stock (Eq, Generic, Ord, Read, Show)
+    | Write T.Text
+    deriving stock (Eq, Generic, Ord, Show)
     deriving anyclass (FromDhall)
