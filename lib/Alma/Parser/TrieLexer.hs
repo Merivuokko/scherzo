831e7df927249735e4b71cb035d3ea3724cafa96
Aura Kelloniemi <kaura.dev@sange.fi>
2022-01-24 15:20:29 +0200

    Fix trie-based lexing for strings with common prefixes

--- lib/Alma/Parser/TrieLexer.hs
+++ lib/Alma/Parser/TrieLexer.hs
@@ -9,14 +9,12 @@ import Data.List.NonEmpty qualified as NL
 import Data.Set qualified as Set
 import Text.Megaparsec
 
-import Alma.Parser.CharTrie as CT
+import Alma.Parser.CharTrie qualified as CT
 import Alma.Parser.Type
 
 parseWithTrie :: forall a. CT.CharTrie a -> Parser a
 {-# INLINEABLE parseWithTrie #-}
-parseWithTrie !trie
-    | CT.null trie = error $! "Programming error: Cannot parse with no tokens"
-    | otherwise = go trie
+parseWithTrie !trie = go trie
   where
     go :: CT.CharTrie a -> Parser a
     {-# INLINEABLE go #-}
@@ -25,25 +23,22 @@ parseWithTrie !trie
         case CT.lookup ch t of
             Just (nextVal, nextTrie) ->
                 anySingle *> case nextVal of
-                    Just res ->
-                        try (go nextTrie) <|> (pure $! res)
-                    Nothing ->
+                    Just res -> do
+                        let recursive = if CT.null nextTrie then empty else try $! go nextTrie
+                        recursive <|> (pure $! res)
+                    Nothing -> do
                         go nextTrie
-            Nothing ->
+            Nothing -> do
                 errorOut ch t
     errorOut :: Char -> CT.CharTrie a -> Parser a
     {-# INLINEABLE errorOut #-}
-    -- Note:return value of errorOut should be a thunk so that it is not
-    -- evaluated in case parsing succeeded on a previous level of the trie.
     errorOut !ch !t
-        = failure (Just $! Tokens $! NL.singleton ch) expectedTokens
+        | CT.null t = error $! "Programming error: No tokens to parse"
+        | otherwise = failure (Just $! Tokens $! NL.singleton ch) expectedTokens
       where
         expectedTokens :: Set.Set (ErrorItem Char)
         {-# INLINEABLE expectedTokens #-}
-        expectedTokens =
-            if CT.null t
-            then error $! "Programming error: Dead end in parsing trie"
-            else Set.fromList $! fmap charToErrorItem $! CT.keys t
+        expectedTokens = Set.fromList $! fmap charToErrorItem $! CT.keys t
         charToErrorItem :: Char -> ErrorItem Char
         {-# INLINEABLE charToErrorItem #-}
         charToErrorItem !c = Tokens $! NL.singleton c
