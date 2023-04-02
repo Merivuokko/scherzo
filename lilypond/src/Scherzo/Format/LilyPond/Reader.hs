7b824026aeb17671376b901b0ce90d6ee7f6cd08
Aura Kelloniemi <kaura.dev@sange.fi>
2023-04-02 18:45:34 +0300

    scherzo-lilypond: New module `Scherzo.Format.LilyPond.Reader`
    This module provides facilities for importing music expressions in GNU LilyPond format into Scherzo music expressions.

---
+++ lilypond/src/Scherzo/Format/LilyPond/Reader.hs
@@ -0,0 +1,34 @@
+-- This module provides an interface for importing music in GNU LilyPond format to Scherzo's music expressions.
+
+-- |
+-- Module      : Scherzo.Format.LilyPond.Reader
+-- Description : LilyPond music importer for Scherzo
+-- Copyright   : Copyright (C) 2023 Aura Kelloniemi
+-- License     : GPL-3
+-- Maintainer  : kaura.dev@sange.fi
+-- Stability   : experimental
+-- Portability : GHC
+module Scherzo.Format.LilyPond.Reader (
+    -- * Importing already loaded data
+    readLilyPond,
+
+    -- * Reading from a file
+    readLilyPondFile,
+) where
+
+import Data.Text qualified as T
+import Data.Text.Encoding
+import System.File.OsPath
+import System.OsPath
+
+import Scherzo.Music.Expr
+
+-- | Parse a LilyPond expression into MusicExpr.
+readLilyPond :: T.Text -> MusicExpr
+readLilyPond = undefined
+
+-- | Read LilyPond expressions from a file, parse it and return as a MusicExpr.
+-- The file is assumed to contain UTF-8 encoded text.
+-- If decoding fails, an exception is thrown.
+readLilyPondFile :: OsPath -> IO MusicExpr
+readLilyPondFile fp = readFile' fp >>= (pure $!) . readLilyPond . decodeUtf8
