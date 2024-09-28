bdd848a640595cfb1063843db13e82f38444db1c
Aura Kelloniemi <kaura.dev@sange.fi>
2024-09-29 01:41:44 +0300

    lilypond: ly2sch: Get LilyPond init file name from the library

--- lilypond/src/ly2sch/main.hs
+++ lilypond/src/ly2sch/main.hs
@@ -2,16 +2,13 @@ module Main (main) where
 
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
-import System.OsPath
 
 import Scherzo.Format.LilyPond.Reader
 
 main :: IO ()
 main = do
-    text <- T.getContents
-    (_, out, err) <-
-        runLilyPondExportSexp
-            [osp|/home/aura/proj/dev/scherzo/lilypond/ly/sexp-export-init.ly|]
-            text
+    input <- T.getContents
+    initFileName <- getLilyPondInitFileName
+    (_, out, err) <- runLilyPondExportSexp initFileName input
     let sexp = readSexp out
     T.putStrLn $! "S-expression:\n" <> (T.pack $ show sexp) <> "\nErrors: " <> err
