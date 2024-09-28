5b62485ef02d09586f57dbadf828bde0bb4a0139
Aura Kelloniemi <kaura.dev@sange.fi>
2024-09-29 00:24:46 +0300

    lilypond: New function `Scherzo.Format.LilyPond.Reader.runLilyPondExportSexp`
    It runs LilyPond with appropriate configuration for extracting music from a
    LilyPond score.

--- lilypond/src/Scherzo/Format/LilyPond/Reader.hs
+++ lilypond/src/Scherzo/Format/LilyPond/Reader.hs
@@ -18,8 +18,12 @@ module Scherzo.Format.LilyPond.Reader (
 
 import Data.Text qualified as T
 import Data.Text.Encoding
+import Data.Text.IO qualified as T
+import System.Exit (ExitCode)
 import System.File.OsPath
+import System.IO (hClose)
 import System.OsPath
+import System.Process
 
 import Scherzo.Music.Expr
 
@@ -32,3 +36,22 @@ readLilyPond = undefined
 -- If decoding fails, an exception is thrown.
 readLilyPondFile :: OsPath -> IO MusicExpr
 readLilyPondFile fp = readFile' fp >>= (pure $!) . readLilyPond . decodeUtf8
+
+-- | Run LilyPond with proper options to convert music into a Scheme expression
+--
+-- Returns the LilyPond processe's exit code, stdout and stderr.
+runLilyPondExportSexp
+    :: OsPath
+    -- ^ Path to the needed init file
+    -> T.Text
+    -- ^ Music in LilyPond format
+    -> IO (ExitCode, T.Text, T.Text)
+runLilyPondExportSexp initFp input = do
+    initString <- decodeFS initFp
+    (stdin, stdout, stderr, ph) <- runInteractiveProcess "lilypond" ["--init", initString, "--loglevel=WARNING", "-"] Nothing Nothing
+    T.hPutStr stdin input
+    hClose stdin
+    outText <- T.hGetContents stdout
+    errText <- T.hGetContents stderr
+    status <- waitForProcess ph
+    pure $! (status, outText, errText)
